c8c825cf2c517b7fecef5324e6e45b3486bcd436
scottvdp <scott@elelsee.com>
2015-10-16 16:24:13 -0500

    cleaning up

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='lambda-customresource',
+      version='0.1',
+      description='AWS Cloudformation Lambda-backed CustomResources in Python',
+      author='Scott VanDenPlas',
+      author_email='scott@elelsee.com',
+      url='https://github.com/elelsee/lambda-customresource',
+      packages=['requests==2.8.1'],
+     )
